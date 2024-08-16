d820648067df0714fcfb67be90657f92285f198d
Walter Moura <walter.moura@sleyersx.com.br>
2024-08-16 18:11:09 -0300

    Atualizações arquivos

--- includes/app.php
+++ includes/app.php
@@ -16,7 +16,7 @@ define("PATH_MAIN", getenv('PATH_MAIN'));
 define("PATH_LOGO_PNG", getenv('PATH_LOGO_PNG'));
 define("PATH_LOGO_ICO", getenv('PATH_LOGO_ICO'));
 define("PATH_LOGO_PNG_IASD", getenv('PATH_LOGO_PNG_IASD'));
-\App\Utils\Debug::debug(URL);
+
 //Define valor padrão das variáveis
 View::init([
     'URL' => URL,
