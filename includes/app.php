ec7543123c2d2bcf7b752ba33650805e68e88277
Walter Moura <walter.moura@sleyersx.com.br>
2024-08-16 17:44:28 -0300

    Atualizações arquivos

--- includes/app.php
+++ includes/app.php
@@ -4,20 +4,20 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Utils\View;
 use App\Utils\Environment;
-use App\Model\Database\Database;
+//use App\Model\Database\Database;
 use App\Http\Middleware\Queue as MiddlewareQueue;
 
 //Carrega variáveis de ambiente
 Environment::load(__DIR__ . '/../../config/env/yb-download/');
 
 // Define as configurações de banco de dados
-Database::config(
-    getenv('DB_HOST'),
-    getenv('DB_NAME'),
-    getenv('DB_USER'),
-    getenv('DB_PASS'),
-    getenv('DB_PORT')
-);
+//Database::config(
+//    getenv('DB_HOST'),
+//    getenv('DB_NAME'),
+//    getenv('DB_USER'),
+//    getenv('DB_PASS'),
+//    getenv('DB_PORT')
+//);
 
 // Dados encryption
 const OPTIONS_BCRYPT = [
@@ -49,45 +49,7 @@ View::init([
 //Define o mapeamento de middlewares
 
 MiddlewareQueue::setMap([
-    'maintenance' => \App\Http\Middleware\Maintenance::class,
-    'required-logout' => \App\Http\Middleware\RequireLogout::class,
-    'required-alert' => \App\Http\Middleware\RequireLogin::class,
-    'required-login' => \App\Http\Middleware\RequireLogin::class,
-    'api' => \App\Http\Middleware\Api::class,
-    'jwe-auth' => \App\Http\Middleware\JWEAuth::class,
-    'session-is-open' => \App\Http\Middleware\SessionOpen::class,
-    'allow-update-passwd' => \App\Http\Middleware\AllowUpdatePasswd::class,
-    'allow-page-advanced-settings' => \App\Http\Middleware\AllowPageAdvancedSettings::class,
-    'allow-page-home' => \App\Http\Middleware\AllowPageHome::class,
-    'allow-page-level' => \App\Http\Middleware\AllowPageManagerLevel::class,
-    'allow-page-type-module' => \App\Http\Middleware\AllowPageManagerTypeModule::class,
-    'allow-page-modules' => \App\Http\Middleware\AllowPageManagerModule::class,
-    'allow-page-access-modules' => \App\Http\Middleware\AllowPageManagerAccessModule::class,
-    'allow-page-manager-user' => \App\Http\Middleware\AllowPageManagerUser::class,
-    'allow-page-manager-settings-smtp' => \App\Http\Middleware\AllowPageManagerSettingsSmtp::class,
-    'allow-page-manager-email-alarmes' => \App\Http\Middleware\AllowPageManagerEmailAlarmes::class,
-    'allow-page-manager-apikey' => \App\Http\Middleware\AllowPageManagerApikey::class,
-    'required-apikey' => \App\Http\Middleware\RequireApikey::class,
-    'allow-page-manager-event' => \App\Http\Middleware\AllowPageManagerEvent::class,
-    'allow-page-config-event' => \App\Http\Middleware\AllowPageConfigEvent::class,
-    'allow-page-status-event' => \App\Http\Middleware\AllowPageStatusEvent::class,
-    'allow-page-departments' => \App\Http\Middleware\AllowPageDepartments::class,
-    'allow-page-programs' => \App\Http\Middleware\AllowPagePrograms::class,
-    'allow-page-elder' => \App\Http\Middleware\AllowPageElder::class,
-    'allow-page-elder-month' => \App\Http\Middleware\AllowPageElderMonth::class,
-    'allow-page-manager-sound-team' => \App\Http\Middleware\AllowPageManagerSoundTeam::class,
-    'allow-page-manager-team' => \App\Http\Middleware\AllowPageManagerTeam::class,
-    'allow-page-device-sound' => \App\Http\Middleware\AllowPageDeviceSound::class,
-    'allow-page-suggested-time' => \App\Http\Middleware\AllowPageSuggestedTime::class,
-    'allow-page-team-lineup' => \App\Http\Middleware\AllowPageTeamLineup::class,
-    'allow-page-ask-to-change' => \App\Http\Middleware\AllowPageAskToChange::class,
-    'allow-page-monitoring' => \App\Http\Middleware\AllowPageMonitoring::class,
-    'allow-page-manager-events-church' => \App\Http\Middleware\AllowPageManagerEventsChurch::class,
-    'allow-page-elder-for-department' => \App\Http\Middleware\AllowPageElderForDepartment::class,
-    'allow-page-manager-temp-users' => \App\Http\Middleware\AllowPageManagerTempUsers::class,
-    'allow-page-approve-temp-user' => \App\Http\Middleware\AllowPageApproveTempUsers::class,
-    'allow-page-reprove-temp-user' => \App\Http\Middleware\AllowPageReproveTempUsers::class,
-    'allow-page-manager-organization' => \App\Http\Middleware\AllowPageManagerOrganization::class
+    'maintenance' => \App\Http\Middleware\Maintenance::class
 ]);
 
 MiddlewareQueue::setDefault([
